c554164ea1c8274617a4e14c44ccc553a3185ae4
Junpei Kawamoto <junpei@mba.local>
2015-06-25 21:49:45 +0900

    Add fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,25 @@
+#
+# fabfile.py
+#
+# Copyright (c) 2015 Junpei Kawamoto
+#
+# This software is released under the MIT License.
+#
+# http://opensource.org/licenses/mit-license.php
+#
 from fabric.api import *
 from fabric.contrib import files
 env.use_ssh_config = True
 
+DIR = "jenkins"
+TAG = "jkawamoto/jenkins"
+
 @task
 def deploy():
     """ Upload contents. """
-    if not files.exists("jenkins"):
-        run("mkdir jenkins")
-    with cd("jenkins"):
+    if not files.exists(DIR):
+        run("mkdir {0}".format(DIR))
+    with cd(DIR):
         put("Dockerfile", ".")
         put("bin", ".", mirror_local_mode=True)
 
@@ -15,5 +27,5 @@ def deploy():
 @task
 def build():
     """ Build a docker image. """
-    with cd("jenkins"):
-        run("docker build -t jkawamoto/jenkins .")
+    with cd(DIR):
+        run("docker build -t {0} .".format(TAG))
